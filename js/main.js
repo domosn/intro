efece5f7d76ddad4dc949847e23272261c1af788
Andy <38588874+domosn@users.noreply.github.com>
2020-03-19 12:38:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+$(function(){
+    $(".back_top").click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'swing');
+    });
 
+    $(document).scroll(function () {
+        var currentY = $(this).scrollTop();
+        if (currentY > 10) {
+            $('.back_top').fadeIn();
+        } else {
+            $('.back_top').fadeOut();
+        }
+    });
+
+});
