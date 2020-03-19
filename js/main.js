10fdd26641ee5d0e8170b90aef53940d01b5b117
Andy <38588874+domosn@users.noreply.github.com>
2020-03-19 14:57:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(function(){
-    $(".back_top").click(function(){
+    $('.work_header').click(function(){
+        if($(this).next().css('display') == 'block') {
+            $('.work_body').slideUp(200, 'linear');
+            return;
+        } else {
+            $('.work_body').slideUp(200, 'linear');
+            $(this).next().slideDown(200, 'linear');
+        }
+    });
+
+    $('.back_top').click(function(){
         $('html, body').animate({
             scrollTop: 0
         }, 'swing');
