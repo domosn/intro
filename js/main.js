5706e41543afa20632767a8de6349fb6b4cfed60
Andy <38588874+domosn@users.noreply.github.com>
2020-03-27 16:20:17 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
+    $('.work_place i').click(function(){
+        let iframeSrc = $(this).data('geo');
+        let iframeElem = "<iframe src='" + iframeSrc + "' width='100%' height='450' frameborder='0' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>";
+        dialogBoxMsg(iframeElem);
+    });
+
     $('img.works_demo').click(function(){
         let imgSrc = $(this).attr('src');
         let imgElem = "<img src='" + imgSrc + "'>";
@@ -34,12 +40,19 @@ $(function(){
 });
 
 function overlay_close() {
-    document.querySelector('.mask').style.display="none";
+    document.querySelector('.intro').style.bottom = '100%';
+    setTimeout(function(){
+        document.querySelector('.mask').style.display="none";
+    }, 500);
+    
 }
 
 function dialogBoxMsg(msg) {
-	str="<div class='close_container'><span class='close' onclick='overlay_close()'></span></div><div class='img_container'>"+msg+"</div></div>";
+	str="<div class='close_container'><span class='close' onclick='overlay_close()'></span></div><div class='ele_container'>"+msg+"</div></div>";
 
 	document.querySelector('.intro').innerHTML=str;
-	document.querySelector('.mask').style.display="flex";
+    document.querySelector('.mask').style.display="flex";
+    setTimeout(function(){
+        document.querySelector('.intro').style.bottom = '0%';
+    }, 100);
 }
