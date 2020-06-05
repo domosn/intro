4dea9802b553a1ee1a2c5dfe21e5e41cccab8539
Andy <38588874+domosn@users.noreply.github.com>
2020-06-05 16:11:55 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('.work_place i').click(function(){
         let iframeSrc = $(this).data('geo');
         let iframeElem = "<iframe src='" + iframeSrc + "' width='100%' height='450' frameborder='0' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>";
@@ -20,6 +19,66 @@ $(function(){
             $('.work_header').removeClass('active').next().slideUp(200, 'linear');
             $(this).addClass('active').next().slideDown(200, 'linear');
         }
+    });
+
+    $('.contactMe').click(function(e){
+		document.querySelector('.contact').style.display='flex';
+		setTimeout(function(){
+			$('.contact').addClass('show');
+			document.querySelector('.contact .form').style.bottom = '10%';
+        }, 100);
+        
+        e.preventDefault();
+    });
+
+    $('.contactSubmit').click(function(){
+        $('.contactResult').html("");
+
+        $("input, textarea").each(function(){
+            $(this).removeClass("error");
+        });
+
+        let err = validateForm();
+
+        if(err != ""){
+            $('.contactResult').html("<div class='submitFailure'>" + err + "</div>");
+            return false
+        }
+
+        let itemArr = [];
+
+        let filltime = getNow();
+		let contactName = $('#contactName').val();
+        let contactEmail = $('#contactEmail').val();
+        let contactMsg = $('#contactMsg').val();
+
+        itemArr.push(filltime);
+        itemArr.push(contactName);
+        itemArr.push(contactEmail);
+        itemArr.push(contactMsg);
+
+        let contactData = {
+            'items': JSON.stringify(itemArr)
+        };
+
+        submitData(contactData);
+        $(".contactSubmit").addClass("processing").prop("disabled", true).html("處理中");
+    });
+
+    $('.contactClose').click(function(){
+		document.querySelector('.contact .form').style.bottom = '20%';
+		setTimeout(function(){
+			$('.contact').removeClass('show');
+		}, 100);
+		setTimeout(function(){
+            document.querySelector('.contact').style.display='none';
+
+            $('.contactResult').html("");
+
+            $("input, textarea").each(function(){
+                $(this).removeClass("error").val("");
+            });
+		}, 300);
     });
 
     $('.back_top').click(function(){
@@ -55,4 +114,98 @@ function dialogBoxMsg(msg) {
     setTimeout(function(){
         document.querySelector('.intro').style.bottom = '0%';
     }, 100);
+}
+
+function submitData(data) {
+	$.ajax({
+		type: "get",
+		url: "https://script.google.com/macros/s/AKfycbyQMlF8CMOr3szv9HuRTt4ALhZXK4_FyvU7CwPdtXyq5XpLw1I/exec",
+		data: data,
+		dataType: "JSON",
+		success: function(response) {
+            $('.contactResult').html("<div class='submitSuccess'>非常感謝您的來信。</div>");
+            $(".contactSubmit").removeClass("processing").prop("disabled", false).html("送出");
+        },
+        error: function(){
+            $('.contactResult').html("<div class='submitFailure'>伺服器目前忙碌中，請稍後再試。</div>");
+            $(".contactSubmit").removeClass("processing").prop("disabled", false).html("送出");
+        }
+	});
+}
+
+function validateForm() {
+    $("input, textarea").each(function(){
+        $(this).removeClass("error");
+    });
+
+    let contactName = $('#contactName').val();
+    let contactEmail = $('#contactEmail').val();
+    let contactMsg = $('#contactMsg').val();
+	let errMsg = "";
+	
+	if(contactName == ""){
+        errMsg += "<div>稱謂 欄位不得為空白。</div>";
+        $('#contactName').addClass("error");
+    }
+    if(contactName.length<2){
+        errMsg += "<div>稱謂 欄位至少需要二個字。</div>";
+        $('#contactName').addClass("error");
+    }
+    if(! /^[\u4e00-\u9fa5_a-zA-Z0-9 ]+$/.test(contactName)){
+        errMsg += "<div>稱謂 欄位只允許使用中文、英文、數字、下劃線(_)及空格字元。</div>";
+        $('#contactName').addClass("error");
+    }
+
+    if(contactEmail == ""){
+        errMsg += "<div>電子信箱 欄位不得為空白。</div>";
+        $('#contactEmail').addClass("error");
+    }
+    if(! /^[\w!#$%&\'*+\/=?^`{|}~.-]+@(?:[a-z\d-]+(?:\.[a-z\d][a-z\d-]*)?)+\.(?:[a-z][a-z\d-]+)$/i.test(contactEmail)){
+        errMsg += "<div>電子信箱 欄位格式不符。</div>";
+        $('#contactEmail').addClass("error");
+    }
+        
+    if (contactMsg == ""){
+        errMsg += "<div>訊息內容 欄位不得為空白。</div>";
+        $('#contactMsg').addClass("error");
+    }
+    if (contactMsg.length<1){
+        errMsg += "<div>訊息內容 欄位至少需要1個字元。</div>";
+        $('#contactMsg').addClass("error");
+    }
+
+	return errMsg;
+}
+
+function getNow(){
+    let currentdate = new Date();
+    let month = currentdate.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let day = currentdate.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    let hour = currentdate.getHours();
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    let minute = currentdate.getMinutes();
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    let second = currentdate.getSeconds();
+    if (second < 10) {
+        second = "0" + second;
+    }
+
+    let nowDate = currentdate.getFullYear() + "/"
+        + month + "/"
+        + day + " "
+        + hour + ":"
+        + minute + ":"
+        + second;
+
+    return nowDate;
 }
